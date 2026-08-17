427238f82a2cce8f17f0ec229c46786b85e2dd3d
Talandar <talandar66@protonmail.com>
2026-08-18 00:07:40 +0200

    2.1 update

--- settings.lua
+++ settings.lua
@@ -1,14 +1,3 @@
-if mods["space-age"] then
-	data:extend({
-		{
-			type = "bool-setting",
-			name = "deepsea-on-gleba",
-			setting_type = "startup",
-			default_value = false,
-			order = "deepsea-mechanic-a",
-		},
-	})
-end
 if not mods["depths_of_nauvis"] then
 	data:extend({
 		{
@@ -16,7 +5,18 @@ if not mods["depths_of_nauvis"] then
 			name = "deepsea-on-nauvis",
 			setting_type = "startup",
 			default_value = false,
-			order = "deepsea-mechanic-c",
+			order = "deepsea-mechanic-a",
+		},
+	})
+end
+if mods["space-age"] then
+	data:extend({
+		{
+			type = "bool-setting",
+			name = "deepsea-on-gleba",
+			setting_type = "startup",
+			default_value = false,
+			order = "deepsea-mechanic-b",
 		},
 	})
 end
